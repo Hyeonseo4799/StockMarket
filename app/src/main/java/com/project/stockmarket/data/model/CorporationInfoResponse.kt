451e12505eee1db7d0befc4ec7967ac597f81331
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-09-07 20:18:01 +0900

    주석 추가 및 변경

--- app/src/main/java/com/project/stockmarket/data/model/CorporationInfoResponse.kt
+++ app/src/main/java/com/project/stockmarket/data/model/CorporationInfoResponse.kt
@@ -17,42 +17,42 @@ data class CorporationInfoResponse(
                 val item: List<Item>
             ) {
                 data class Item(
-                    val actnAudpnNm: String,
-                    val audtRptOpnnCtt: String,
-                    val basDt: String,
-                    val bzno: String,
-                    val corpDcd: String,
-                    val corpDcdNm: String,
-                    val corpEnsnNm: String,
-                    val corpNm: String,
-                    val corpRegMrktDcd: String,
-                    val corpRegMrktDcdNm: String,
-                    val crno: String,
-                    val empeAvgCnwkTermCtt: String,
-                    val enpBsadr: String,
-                    val enpDtadr: String,
-                    val enpEmpeCnt: String,
-                    val enpEstbDt: String,
-                    val enpFxno: String,
-                    val enpHmpgUrl: String,
-                    val enpKosdaqLstgAbolDt: String,
-                    val enpKosdaqLstgDt: String,
-                    val enpKrxLstgAbolDt: String,
-                    val enpKrxLstgDt: String,
-                    val enpMainBizNm: String,
-                    val enpMntrBnkNm: String,
-                    val enpOzpno: String,
-                    val enpPbanCmpyNm: String,
-                    val enpPn1AvgSlryAmt: String,
-                    val enpRprFnm: String,
-                    val enpStacMm: String,
-                    val enpTlno: String,
-                    val enpXchgLstgAbolDt: String,
-                    val enpXchgLstgDt: String,
-                    val fssCorpChgDtm: String,
-                    val fssCorpUnqNo: String,
-                    val sicNm: String,
-                    val smenpYn: String
+                    val actnAudpnNm: String,        // 회계감사인명
+                    val audtRptOpnnCtt: String,     // 감사보고서 의견내용
+                    val basDt: String,              // 기준일
+                    val bzno: String,               // 사업자등록번호
+                    val corpDcd: String,            // 법인구분코드
+                    val corpDcdNm: String,          // 법인구분코드명
+                    val corpEnsnNm: String,         // 법인영문명
+                    val corpNm: String,             // 법인명
+                    val corpRegMrktDcd: String,     // 법인등록시장구분코드
+                    val corpRegMrktDcdNm: String,   // 법인등록시장구분코드명
+                    val crno: String,               // 법인등록번호
+                    val empeAvgCnwkTermCtt: String, // 종업원평균근속기간내용
+                    val enpBsadr: String,           // 기업기본주소
+                    val enpDtadr: String,           // 기업상세주소
+                    val enpEmpeCnt: String,         // 기업종업원수
+                    val enpEstbDt: String,          // 기업설립일자
+                    val enpFxno: String,            // 기업팩스번호
+                    val enpHmpgUrl: String,         // 기업홈페이지URL
+                    val enpKosdaqLstgAbolDt: String,// 기업코스닥상장폐지일자
+                    val enpKosdaqLstgDt: String,    // 기업코스닥상장일자
+                    val enpKrxLstgAbolDt: String,   // 기업KONEX상장폐지일자
+                    val enpKrxLstgDt: String,       // 기업KONEX상장일자
+                    val enpMainBizNm: String,       // 기업주요사업명
+                    val enpMntrBnkNm: String,       // 기업주거래은행명
+                    val enpOzpno: String,           // 기업구우편번호
+                    val enpPbanCmpyNm: String,      // 기업공시회사명
+                    val enpPn1AvgSlryAmt: String,   // 기업1인평균급여금액
+                    val enpRprFnm: String,          // 기업대표자성명
+                    val enpStacMm: String,          // 기업결산월
+                    val enpTlno: String,            // 기업전화번호
+                    val enpXchgLstgAbolDt: String,  // 기업거래소상장폐지일자
+                    val enpXchgLstgDt: String,      // 기업거래소상장일자
+                    val fssCorpChgDtm: String,      // 금융감독원법인변경일시
+                    val fssCorpUnqNo: String,       // 금융감독원법인고유번호
+                    val sicNm: String,              // 표준산업분류명
+                    val smenpYn: String             // 중소기업여부
                 )
             }
         }
--- app/src/main/java/com/project/stockmarket/data/model/StockPriceInfoResponse.kt
+++ app/src/main/java/com/project/stockmarket/data/model/StockPriceInfoResponse.kt
@@ -17,21 +17,21 @@ data class StockPriceInfoResponse(
                 val item: List<Item>
             ) {
                 data class Item(
-                    val basDt: String,
-                    val clpr: String,
-                    val fltRt: String,
-                    val hipr: String,
-                    val isinCd: String,
-                    val itmsNm: String,
-                    val lopr: String,
-                    val lstgStCnt: String,
-                    val mkp: String,
-                    val mrktCtg: String,
-                    val mrktTotAmt: String,
-                    val srtnCd: String,
-                    val trPrc: String,
-                    val trqu: String,
-                    val vs: String
+                    val basDt: String,      // 기준일
+                    val clpr: String,       // 종가
+                    val fltRt: String,      // 전일 대비 등락 비율
+                    val hipr: String,       // 고가
+                    val isinCd: String,     // 국제 채권 식별 번호
+                    val itmsNm: String,     // 주식회사명
+                    val lopr: String,       // 저가
+                    val lstgStCnt: String,  // 상장 주식수
+                    val mkp: String,        // 시초가
+                    val mrktCtg: String,    // 시장 구분 (코스피/코스닥/코넥스)
+                    val mrktTotAmt: String, // 시가총액
+                    val srtnCd: String,     // 종목 코드
+                    val trPrc: String,      // 거래대금
+                    val trqu: String,       // 거래량
+                    val vs: String          // 전일 대비 등락
                 )
             }
         }
