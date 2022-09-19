3cac95aeb348d0f51169e398a14ef9ea9aa97103
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-09-19 17:26:57 +0900

    Retrofit Client 설정

--- app/src/main/java/com/project/stockmarket/di/AppModule.kt
+++ app/src/main/java/com/project/stockmarket/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.project.stockmarket.di
 
+import com.project.stockmarket.common.Constants
 import com.project.stockmarket.data.api.StockInfoApi
 import com.project.stockmarket.data.repository.StockInfoRepositoryImpl
 import com.project.stockmarket.domain.repository.StockInfoRepository
@@ -7,6 +8,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,10 +21,19 @@ object AppModule {
     @Singleton
     fun provideStockInfoApi(): StockInfoApi {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(Constants.baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkhttpClient())
             .build()
             .create(StockInfoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        val httpinterceptor = HttpLoggingInterceptor()
+        httpinterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(httpinterceptor).build()
     }
 
     @Provides
