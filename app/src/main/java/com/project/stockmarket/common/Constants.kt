03fe03a2faaeabf2bcafa3a3791e00f0a9fd7e32
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-09-20 18:14:03 +0900

    API KEY 은닉

--- app/src/main/java/com/project/stockmarket/common/Constants.kt
+++ app/src/main/java/com/project/stockmarket/common/Constants.kt
@@ -2,5 +2,4 @@ package com.project.stockmarket.common
 
 object Constants {
     const val baseUrl = "http://apis.data.go.kr/"
-    const val serviceKey = "sWdrFPrZ3yGvhRfn3UGF2NevoFaJi2LsuGZgExZMHfn9XUrE5fBI8MmYEaFGVmsDkaT01Q41RCEe9PpLqO%2FbOw%3D%3D"
 }
