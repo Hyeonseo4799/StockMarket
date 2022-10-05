341efaec9b9184e9b020b5fef2e9c434f2c906d0
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-10-05 21:14:23 +0900

    Card View 구현

--- app/src/main/java/com/project/stockmarket/presentation/view/DetailScreen.kt
+++ app/src/main/java/com/project/stockmarket/presentation/view/DetailScreen.kt
@@ -1,14 +1,6 @@
 package com.project.stockmarket.presentation.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.project.stockmarket.domain.model.StockPriceInfo
@@ -27,39 +19,32 @@ fun DetailScreen(
     val stockIssuanceInfoState = viewModel.stockIssuanceInfoState.value
     val koreaStandardIndustryCodeState = viewModel.koreaStandardIndustryCodeState.value
 
+    viewModel.getKoreaStandardIndustryCode()
     viewModel.getStockPriceInfo(isinCode)
     viewModel.getCorporationInfo(corpNumber)
     viewModel.getStockIssuanceInfo(corpNumber)
-    viewModel.getKoreaStandardIndustryCode()
 
-    DetailView(state = stockPriceInfoState, navController = navController)
-}
-
-@Composable
-fun DetailView(state: DataState<StockPriceInfo>, navController: NavController) {
-    if (state.data.isNotEmpty()) {
-        TopBar(text = state.data[0].stockName, content = { Content() }) {
-            navController.popBackStack()
+    if (corporationInfoState.data.isNotEmpty()) {
+        val industryCode = koreaStandardIndustryCodeState.data.filter {
+            it.industryCode == corporationInfoState.data[0].IndustryCode
         }
+        if (industryCode.isNotEmpty())
+            CardView(stockPriceInfoState, industryCode[0].industryClassification, navController)
     }
+    StateView(state = listOf(corporationInfoState, stockPriceInfoState))
 }
 
 @Composable
-fun Content() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF9F9F9))
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .padding(start = 20.dp, end = 20.dp, top = 26.dp, bottom = 26.dp)
-                .shadow(5.dp, RoundedCornerShape(8.dp))
-                .background(MaterialTheme.colors.background, RoundedCornerShape(8.dp))
-        ) {
-
+fun CardView(
+    stockPriceInfoState: DataState<StockPriceInfo>,
+    industryCode: String,
+    navController: NavController
+) {
+    if (stockPriceInfoState.data.isNotEmpty()) {
+        TopBar(text = stockPriceInfoState.data[0].stockName, content = {
+            Content(stockPriceInfoState.data[0], industryCode)
+        }) {
+            navController.popBackStack()
         }
     }
 }
