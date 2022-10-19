a0a44c4491caa0cb56b4c65eeacd257ca80c4d7c
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-10-19 14:54:56 +0900

    DB_NAME 추가

--- app/src/main/java/com/project/stockmarket/Constants.kt
+++ app/src/main/java/com/project/stockmarket/Constants.kt
@@ -3,4 +3,5 @@ package com.project.stockmarket
 object Constants {
     const val BASE_URL = "http://apis.data.go.kr/"
     const val KSIC_URL = "https://api.odcloud.kr/api/15049591/v1/uddi:5e38a64c-9482-4cda-8169-860015fa3406/"
+    const val DB_NAME = "ksic_db"
 }
